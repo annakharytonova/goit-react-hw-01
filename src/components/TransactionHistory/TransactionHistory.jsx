2c7679326ab055709a52a7836806793d45657122
Anna Kharytonova <haritonova1512@gmail.com>
2025-01-12 22:01:32 +0200

    add TransactionHistory

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import css from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.thead}>
+        <tr>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody className={css.tbody}>
+        {transactions.map((item) => (
+          <tr key={item.id} className={css.tr}>
+            <td className={css.td}>{item.type}</td>
+            <td className={css.td}>{item.amount}</td>
+            <td className={css.td}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
